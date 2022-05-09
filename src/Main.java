35c02ea1b09af064963b482deb7334a937bebeb7
bgs-study <qkrwlgp235@gmail.com>
2022-05-09 15:55:20 +0900

    Menu Layout Change and Title Logo ADD

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 class Menu extends JFrame{
@@ -12,32 +21,41 @@ class Menu extends JFrame{
 		
 		//컨테이너 ------------------------------------------------
 		Container c = getContentPane();
-		c.setLayout(null);
+		c.setLayout(new BorderLayout());
 		
 		//JPanner------------------------------------------------
 		JPanel p_background = new JPanel();
-		p_background.setLayout(null);
+		p_background.setLayout(new GridLayout(2,2));
 		p_background.setBackground(Color.white);
 		p_background.setBounds(0, 0, 600, 800);
-		c.add(p_background);
+		c.add(p_background,BorderLayout.CENTER);
 		
-		JPanel p_north = new JPanel();
-		p_north.setLayout(null);
+		JPanel p_north = new JPanel();		
+		p_north.setLayout(new FlowLayout());
 		p_north.setBackground(new Color(255, 158, 60));
 		p_north.setBounds(0, 0, 600, 80);
-		p_background.add(p_north);
+		c.add(p_north,BorderLayout.NORTH);
+		//p_background.add(p_north);
+		
+		JPanel p_south = new JPanel();	
+		p_south.setLayout(new FlowLayout());
+		p_south.setBackground(new Color(255, 158, 60));
+		p_south.setBounds(0, 0, 600, 80);
+		c.add(p_south,BorderLayout.SOUTH);
 		
 
 		//JLabel-------------------------------------------------
 		
-		JLabel title = new JLabel("동국 킹 버거");
+		JLabel title = new JLabel("동국 킹 버거");	
+		ImageIcon logo_origin = new ImageIcon("./images/img/logo_transparent.png");		
+		JLabel title_logo = new JLabel(logo_origin); 
+		
+		p_north.add(title_logo);
+		
 		title.setBounds(10, 15, 300, 50); //위치
 		title.setForeground(Color.white); //글자 색
 		title.setFont(new Font("Dailog", Font.BOLD, 50)); //글자 폰트, 크기
 		p_north.add(title);
-		
-		
-		//
 		
 		
 		//주문하기, 취소하기 버튼 ----------------------------------------
@@ -48,7 +66,7 @@ class Menu extends JFrame{
 		order_btn.setForeground(Color.white); // 배경 색
 		order_btn.setBorder(BorderFactory.createLineBorder(Color.white)); //버튼 테두리 색
 		order_btn.setFont(new Font("Dialog", Font.BOLD, 40)); // 글자 폰트 및 크기
-		p_background.add(order_btn);
+		p_south.add(order_btn, BorderLayout.WEST);
 		
 		JButton cancel_btn = new JButton("취소하기");
 		cancel_btn.setBounds(300, 665, 300, 100);//위치
@@ -56,7 +74,7 @@ class Menu extends JFrame{
 		cancel_btn.setForeground(Color.white); // 배경 색
 		cancel_btn.setBorder(BorderFactory.createLineBorder(Color.white));//버튼 테두리 색
 		cancel_btn.setFont(new Font("Dialog", Font.BOLD, 40)); // 글자 폰트 및 크기
-		p_background.add(cancel_btn);
+		p_south.add(cancel_btn,BorderLayout.EAST);
 		
 		
 		//이벤트, 결제 팝업창 다이어로그 생성-------------------------------------------
